59e1eae1febdb4404ce385c304a0c6dbd719dec0
Sergio <oliveserg@gmail.com>
2016-08-31 20:09:03 +0000

    fix

--- public/mobile.js
+++ public/mobile.js
@@ -1,7 +1,15 @@
 function setup(){
-
+  createCanvas(displayWidth, displayHeight);
+	strokeWeight(10)
+	stroke(0);
 }
 
-function draw(){
-  
+function touchMoved(){
+  line(touchX, touchY, ptouchX, ptouchY);
+	return false;
 }
+
+$(".post").on('submit', function(ev){
+  ev.preventDefault()
+  var image = $(".canvas").getDataUrl()
+})
