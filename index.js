55a57db29b236e4c333a675f3586433371bcc470
gingerguo <guomimai@gmail.com>
2016-08-31 18:07:09 +0000

    2

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    // console.log('message: ' + msg);
+  });
+
+});
