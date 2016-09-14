a26ff5b95af3a02e885c30a30c5958e23f88029b
gingerguo <guomimai@gmail.com>
2016-09-14 13:43:41 +0000

    windowdisplay

--- public/mobile.js
+++ public/mobile.js
@@ -7,7 +7,6 @@ var saveTime = 1;
 var rand = [];
 
 function setup(){
-  // createCanvas(displayWidth, displayHeight);
   createCanvas(windowWidth, windowHeight);
 	strokeWeight(10);
 	stroke(0);
@@ -19,10 +18,7 @@ function setup(){
 }
 
 function draw(){
-  // setGradient(0, 0, displayWidth, displayHeight, c1, c2);
   setGradient(0, 0, windowWidth, windowHeight, c1, c2);
-
-
   if(millis() - saveTime > 200){
     step += 1;
     saveTime = millis();
